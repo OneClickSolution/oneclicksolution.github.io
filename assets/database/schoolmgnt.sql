a427f50ce4c5913baf7dcff5145a327d79e7edbc
Archana Kushwaha <141910200+OneClickSolution@users.noreply.github.com>
2023-08-10 14:30:41 +0530

    One Click Solution
    Welcome to our website! We are a passionate and dedicated team of professionals committed towards providing you with best solutions for your needs. With our expertise of 5 years, we aim to build a long-lasting relationship. Our aim is to bring your vision to life and make an event a success Big brands turn to ONE CLICK SOLUTION when they are looking forward in search of Event planner,production house empower ,content creators.
    
    We offer a wide range of event planning services to meet your specific necessities wether it’s a birthday party, wedding party or any other special occasion. We’re also passionate about creating captivating and inspiring content that brings story to your life, A team of talented professionals, will tirelessly look forward to cater your needs. We specialise in producing high quality films, videos, web series, short films. What sets us apart is our commitment towards innovation and creativity.
    
    Lastly, our platform is dedicated to empower creators of all kinds to showcase their talent and reach out to a broader audience. Our platform enables creators to share their content pachas videos, articles and art.
    
    Thank you for choosing One Click Solution. Together, Let's create moments That will be cherished forever .Get in touch Get in touch with us to Discuss your next project, and let’s bring your ideas to life.

---
+++ assets/database/schoolmgnt.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2023 at 06:21 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `schoolmgnt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(20) NOT NULL,
+  `staff_name` varchar(50) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `best_subject` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
